06a843ea5fc4eae518767e9089cee6f4a3faef9b
dbahuydang <44718845+dbahuydang@users.noreply.github.com>
2018-11-04 16:52:14 +0700

    dsadsa

--- Tables/dbo.Test.sql
+++ Tables/dbo.Test.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Test]
 (
 [c1] [bigint] NULL,
-[c2] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[c2] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[dkm3] [int] NULL
 ) ON [PRIMARY]
 GO
